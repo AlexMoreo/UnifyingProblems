27a3db686539645e0f496c6e69ab34e357cf497f
Alejandro Moreo <alejandro.moreo@isti.cnr.it>
2025-03-04 14:02:59 +0100

    adding sh to generate everything

---
+++ neural_training/generate_data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for dataset in yelp imdb ag_news ; do
+  for model in google-bert/bert-base-uncased distilbert/distilbert-base-uncased FacebookAI/roberta-base ; do
+    CUDA_VISIBLE_DEVICES=1 python train_and_embed.py --nepochs 5 --model_name $model --dataset_name $dataset --val_size 5000
+  done
+done
+
+
+
