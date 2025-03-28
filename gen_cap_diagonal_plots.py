d744bbad9c6c64c9d41ba33019d4b06b9b1bdc93
Alejandro Moreo <alejandro.moreo@isti.cnr.it>
2025-03-28 18:31:05 +0100

    adding diagonal plots for cap

--- gen_cap_diagonal_plots.py
+++ gen_cap_diagonal_plots.py
@@ -1,38 +1,54 @@
-import itertools
 import os
 from glob import glob
+from itertools import product
+
 import pandas as pd
-from quapy.method.aggregative import KDEyML
-from quapy.protocol import UPP
-from sklearn.base import BaseEstimator
-from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.neural_network import MLPClassifier
-
-import util
 import matplotlib.pyplot as plt
-import seaborn as sns
 from pathlib import Path
-import numpy as np
-import quapy as qp
-from commons import uci_datasets
 from model.classifier_accuracy_predictors import *
-from commons import SAMPLE_SIZE, REPEATS
-from tqdm import tqdm
 
 
-def binary_diagonal(method_names, true_prevs, estim_prevs, pos_class=1, title=None, show_std=True, legend=True,
-                    train_prev=None, savepath=None, method_order=None, num_bins=10):
+def included_method(file_path, include_methods):
+    name = Path(file_path).name.split('_')[0]
+    for method in include_methods:
+        if method == name:
+            return True
+    return False
+
+
+def included_dataset(file_path, dataset=None):
+    if dataset is not None:
+        return dataset in file_path
+    return True
+
+def include_cls(file_path:str, classifier):
+    if file_path.endswith(classifier+'.csv'):
+        return True
+    if len(classifier)>5 and classifier in file_path:
+        return True
+    return False
+
+
+def include_result(file_path, include_methods, dataset, classifier):
+    if not included_method(file_path, include_methods):
+        return False
+    if not included_dataset(file_path, dataset):
+        return False
+    if not include_cls(file_path, classifier):
+        return False
+    return True
+
+
+def cap_diagonal(method_names, true_acc, estim_acc, naive=None, title=None, legend=True, savepath=None, method_order=None):
 
     fig, ax = plt.subplots()
     ax.set_aspect('equal')
     ax.grid()
     ax.plot([0, 1], [0, 1], '--k', label='ideal', zorder=1)
 
-    order = list(zip(method_names, true_prevs, estim_prevs))
+    order = list(zip(method_names, true_acc, estim_acc))
     if method_order is not None:
-        table = {method_name:[true_prev, estim_prev] for method_name, true_prev, estim_prev in order}
+        table = {method_name:[true_acc, estim_acc] for method_name, true_acc, estim_acc in order}
         order  = [(method_name, *table[method_name]) for method_name in method_order]
 
     NUM_COLORS = len(method_names)
@@ -40,30 +56,16 @@ def binary_diagonal(method_names, true_prevs, estim_prevs, pos_class=1, title=No
         cm = plt.get_cmap('tab20')
         ax.set_prop_cycle(color=[cm(1. * i / NUM_COLORS) for i in range(NUM_COLORS)])
 
-    bin_edges = np.linspace(0, 1, num_bins + 1)
-    bin_centers = 0.5 * (bin_edges[:-1] + bin_edges[1:])
+    if naive is not None:
+        ax.axhline(y=0.5, color="gray", linestyle="-.", linewidth=1.5, label="Naive")
 
-    for method, true_prev, estim_prev in order:
-        bin_indices = np.digitize(true_prev[:,pos_class], bins=bin_edges, right=True)
+    for method, true_acc, estim_acc in order:
+        ax.scatter(x=true_acc, y=estim_acc, marker='o', label=method, zorder=2, alpha=0.5)
 
-        estim_prev = estim_prev[:,pos_class]
-
-        y_ave = np.array([estim_prev[bin_indices == i].mean() for i in range(1, num_bins + 1)])
-        y_std = np.array([estim_prev[bin_indices == i].std() for i in range(1, num_bins + 1)])
-
-        ax.errorbar(bin_centers, y_ave, fmt='-', marker='o', label=method, markersize=3, zorder=2)
-
-        if show_std:
-            ax.fill_between(bin_centers, y_ave - y_std, y_ave + y_std, alpha=0.1)
-
-    if train_prev is not None:
-        train_prev = train_prev[pos_class]
-        ax.scatter(train_prev, train_prev, c='c', label='tr-prev', linewidth=2, edgecolor='k', s=100, zorder=3)
-
-
-    ax.set(xlabel='true prevalence', ylabel='estimated prevalence', title=title)
+    ax.set(xlabel='true Accuracy', ylabel='estimated Accuracy', title=title)
     ax.set_ylim(0, 1)
     ax.set_xlim(0, 1)
+
 
     if legend:
         ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
@@ -73,89 +75,63 @@ def binary_diagonal(method_names, true_prevs, estim_prevs, pos_class=1, title=No
 
 task = 'classifier_accuracy_prediction'
 dataset_shift = 'label_shift'
+# dataset_shift = 'covariate_shift'
 
+input_dir = f'./results/{task}/{dataset_shift}/repeats_100_samplesize_250'
 
+if dataset_shift == 'label_shift':
+    include_methods = [
+        'Naive',
+        'ATC',
+        'DoC',
+        'LEAP',
+        'Cpcs-a-S',
+        'PACC-a'
+    ]
 
-def cap_methods(h:BaseEstimator, Xva, yva):
+    classifiers=['lr', 'knn', 'nb', 'mlp']
 
-    # CAP methods
-    yield 'Naive', NaiveIID(classifier=h).fit(Xva, yva)
-    yield 'ATC', ATC(h).fit(Xva, yva)
-    val_prot = UPP(val, sample_size=SAMPLE_SIZE, repeats=REPEATS, random_state=0, return_type='labelled_collection')
-    yield 'DoC', DoC(h, protocol=val_prot).fit(Xva, yva)
-    yield 'LEAP', LEAP(classifier=h, q_class=KDEyML(classifier=h)).fit(Xva, yva)
+    datasets = ['cmc.3']
 
+else:
+    include_methods = [
+        'Naive',
+        'ATC',
+        'DoC',
+        'LEAP',
+        'Cpcs-a-S'
+    ]
 
-    # Calibration 2 CAP
-    yield 'TransCal-a-S', CalibratorCompound2CAP(classifier=h, calibrator_cls=TransCalCalibrator, probs2logits=True, Ftr=Xtr, ytr=ytr).fit(Xva, yva)
-    # yield 'Cpcs-a-S', CalibratorCompound2CAP(classifier=h, calibrator_cls=CpcsCalibrator, probs2logits=True, Ftr=Xtr, ytr=ytr).fit(Xva, yva)
-    yield 'LasCal-a-P', LasCal2CAP(classifier=h, probs2logits=False).fit(Xva, yva)
+    classifiers = ['distilbert-base-uncased']
 
-    # Quantification 2 CAP
-    yield 'PACC-a', Quant2CAP(classifier=h, quantifier_class=PACC).fit(Xva, yva)
-    yield 'KDEy-a', Quant2CAP(classifier=h, quantifier_class=KDEyML).fit(Xva, yva)
-    yield 'EMQ-a', Quant2CAP(classifier=h, quantifier_class=EMQ).fit(Xva, yva)
+    datasets = ['imdb__rt']
 
+for dataset, classifier in product(datasets, classifiers):
 
-def classifiers():
-    yield 'lr', LogisticRegression()
-    yield 'nb', GaussianNB()
-    yield 'knn', KNeighborsClassifier(n_neighbors=10, weights='uniform')
-    yield 'mlp', MLPClassifier()
+    results = pd.concat([pd.read_csv(result_file) for result_file in glob(input_dir + '/*.csv') if include_result(result_file, include_methods, dataset, classifier)])
 
-pbar = tqdm(itertools.product(datasets_selected, classifiers()), total=len(datasets_selected) * n_classifiers)
-for dataset, (cls_name, h) in pbar:
-    pbar.set_description(f'running: {dataset}')
+    method_names, true_acc, estim_acc = [], [], []
 
-    data = qp.datasets.fetch_UCIBinaryDataset(dataset)
-    train, test = data.train_test
-    train_prev = train.prevalence()
-    train, val = train.split_stratified(0.5, random_state=0)
-
-    Xtr, ytr = train.Xy
-    h.fit(Xtr, ytr)
-
-    Xva, yva = val.Xy
-
-    # sample generation protocol ("artificial prevalence protocol" -- generates prior probability shift)
-    app = UPP(test, sample_size=SAMPLE_SIZE, repeats=REPEATS, return_type='labelled_collection')
-
-    for name, cap_method in cap_methods(h, Xva, yva):
-        if name not in methods_order:
-            methods_order.append(name)
-
-        result_method_dataset_path = join(result_dir, f'{name}_{dataset}_{cls_name}.csv')
-        if os.path.exists(result_method_dataset_path):
-            report = pd.read_csv(result_method_dataset_path)
+    naive=None
+    for method in results.method.unique():
+        df_sel = results[results['method'] == method]
+        if method == 'Naive':
+            naive = df_sel['estim_acc'].values[0]
         else:
-            method_dataset_results = []
-            for id, test_shifted in tqdm(enumerate(app()), total=app.total(), desc=f'model={name}-h={cls_name}'):
-                Xte, yte = test_shifted.Xy
+            method_names.append(method)
+            true_acc.append(df_sel['true_acc'].values)
+            estim_acc.append(df_sel['estim_acc'].values)
 
-                y_pred = h.predict(Xte)
-                acc_true = accuracy(y_true=yte, y_pred=y_pred)
-
-                acc_estim = cap_method.predict(Xte)
-                err_cap = cap_error(acc_true=acc_true, acc_estim=acc_estim)
-
-                shift = qp.error.ae(test_shifted.prevalence(), train_prev)
-                result = ResultRow(dataset=dataset, id=id, method=name, classifier=cls_name, shift=shift,
-                                   err=err_cap)
-                method_dataset_results.append(asdict(result))
-
-            report = pd.DataFrame(method_dataset_results)
-            report.to_csv(result_method_dataset_path, index=False)
-
-        all_results.append(report)
-
-    path = f'figures/diagonal/quantification_{dataset}.pdf'
-    binary_diagonal(
+    if dataset is None:
+        dataset='all'
+    path = f'figures/diagonal/{task}/{dataset_shift}_{dataset}_{classifier}.png'
+    if 'Naive' in include_methods:
+        include_methods.remove('Naive')
+    cap_diagonal(
         method_names=method_names,
-        true_prevs=true_prevs,
-        estim_prevs=estim_prevs,
-        show_std=True,
+        true_acc=true_acc,
+        estim_acc=estim_acc,
+        naive=naive,
         savepath=path,
-        train_prev=train_prev,
         method_order=include_methods,
-        num_bins=15
     )
